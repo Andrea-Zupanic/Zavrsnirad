0bf0862f5dbfb9e2527a9c7c127c43d2ee7bf9fe
Andrea Županić <andrea.zupanic10@gmail.com>
2024-01-29 22:29:43 +0100

    Pocetak CSharp Konjicki Klub

---
+++ KonjickiKlubCS/KonjickiKlubCS/Korisnik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonjickiKlubCS
+{
+    internal class Korisnik : Osoba
+    {
+        public int BrojMobitela { get; set; }
+
+        public bool Iskustvo { get; set; }
+
+        public string GodinaRođenja { get; set; }
+
+    }
+}
---
+++ KonjickiKlubCS/KonjickiKlubCS/Usluga.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonjickiKlubCS
+{
+    internal class Usluga : Entitet
+    {
+
+        public string Naziv { get; set; }
+
+        public int BrojSati { get; set; }
+
+        public int Cijena { get; set; }
+
+        public bool PotrebnoIskustvo { get; set; }
+
+
+    }
+}
